e6619063e995f159f02e99f58ea70e0a3b97b4c3
Tiago Freire <tmiguelf@hotmail.com>
2021-10-07 13:51:42 +0200

    Made picked algorithms adaptative to the CPU flags at runtime.

--- Crypt/src/hash/crc.cpp
+++ Crypt/src/hash/crc.cpp
@@ -30,10 +30,12 @@
 
 #include <CoreLib/Core_Type.hpp>
 #include <CoreLib/Core_Endian.hpp>
+#include <CoreLib/Core_cpu.hpp>
+
 #include <Crypt/utils.hpp>
 
 
-#if defined(_M_AMD64) || defined(__SSE4_2__)
+#if defined(_M_AMD64) || defined(__amd64__)
 #include <nmmintrin.h>
 #else
 #endif
@@ -166,189 +168,224 @@ namespace
 			//else //TODO
 		}
 	};
-}
 
-
-#if defined(_M_AMD64) || defined(__SSE4_2__)
-uint32_t CRC_32C::trasform(uint32_t p_current, const std::span<const uint8_t> p_data)
-{
-	const uintptr_t			size  = p_data.size();
-	const uint8_t*			pivot = p_data.data();
-	const uint8_t* const	last  = pivot + size;
-
+	struct CRC_32C_Help
 	{
-		const uintptr_t off_align = align_t_mod<uint64_t>(pivot);
-		if((alignof(uint64_t) - off_align) > size)
+		static CRC_32C::digest_t trasform_u_soft(CRC_32C::digest_t p_current, const std::span<const uint8_t> p_data)
 		{
-			for(const uint8_t tpoint : p_data)
+			static_assert(std::is_same_v<CRC_32C::digest_t, uint32_t>);
+			using CRC_Helper = CRC_Help<CRC_32C::digest_t, CRC_32C::poly, CRC_32C::reciprocal>;
+
+			const uintptr_t			size  = p_data.size();
+			const uint8_t*			pivot = p_data.data();
+			const uint8_t* const	last  = pivot + size;
+
 			{
-				p_current = _mm_crc32_u8(p_current, tpoint);
+				const uintptr_t off_align = align_t_mod<uint32_t>(pivot);
+				if((alignof(uint32_t) - off_align) > size)
+				{
+					for(const uint8_t tpoint : p_data)
+					{
+						p_current = CRC_Helper::soft_byte(p_current, tpoint);
+					}
+					return p_current;
+				}
+
+				switch(off_align)
+				{
+				case 1:
+					p_current = CRC_Helper::soft_byte(p_current, *pivot++);
+					[[fallthrough]];
+				case 2:
+					p_current = CRC_Helper::soft_byte(p_current, *pivot++);
+					[[fallthrough]];
+				case 3:
+					p_current = CRC_Helper::soft_byte(p_current, *pivot++);
+					break;
+				default:
+				case 0:
+					break;
+				}
+			}
+
+			for(; (last - pivot) >= 4; pivot += 4)
+			{
+				p_current = CRC_Helper::soft_multi_byte(p_current, *reinterpret_cast<const uint32_t*>(pivot));
+			}
+
+			switch(last - pivot)
+			{
+			case 3:
+				p_current = CRC_Helper::soft_byte(p_current, *pivot++);
+				[[fallthrough]];
+			case 2:
+				p_current = CRC_Helper::soft_byte(p_current, *pivot++);
+				[[fallthrough]];
+			case 1:
+				p_current = CRC_Helper::soft_byte(p_current, *pivot);
+				break;
+			default:
+			case 0:
+				break;
+			}
+
+			return p_current;
+		}
+
+		static CRC_32C::digest_t trasform_a_soft(CRC_32C::digest_t p_current, const std::span<const uint64_t> p_data)
+		{
+			static_assert(std::is_same_v<CRC_32C::digest_t, uint32_t>);
+			using CRC_Helper = CRC_Help<CRC_32C::digest_t, CRC_32C::poly, CRC_32C::reciprocal>;
+			for(const uint64_t tpoint : p_data)
+			{
+				p_current = CRC_Helper::soft_multi_byte(p_current, *reinterpret_cast<const uint32_t*>(&tpoint));
+				p_current = CRC_Helper::soft_multi_byte(p_current, *(reinterpret_cast<const uint32_t*>(&tpoint) + 1));
 			}
 			return p_current;
 		}
 
-		switch(off_align)
+		
+#if defined(_M_AMD64) || defined(__amd64__)
+		static uint32_t trasform_u_intri(uint32_t p_current, const std::span<const uint8_t> p_data)
 		{
-		case 1:
-			p_current = _mm_crc32_u8(p_current, *pivot++);
-			[[fallthrough]];
-		case 2:
-			p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
-			pivot += 2;
-			[[fallthrough]];
-		case 4:
-			p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
-			pivot += 4;
-			break;
-		case 3:
-			p_current = _mm_crc32_u8(p_current, *pivot++);
-			p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
-			pivot += 4;
-			break;
-		case 5:
-			p_current = _mm_crc32_u8(p_current, *pivot++);
-			[[fallthrough]];
-		case 6:
-			p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
-			pivot += 2;
-			break;
-		case 7:
-			p_current = _mm_crc32_u8(p_current, *pivot++);
-			break;
-		default:
-		case 0:
-			break;
-		}
-	}
+			const uintptr_t			size  = p_data.size();
+			const uint8_t*			pivot = p_data.data();
+			const uint8_t* const	last  = pivot + size;
 
-	{
-		uint64_t context64 = p_current;
-		for(; (last - pivot) >= 8; pivot += 8)
+			{
+				const uintptr_t off_align = align_t_mod<uint64_t>(pivot);
+				if((alignof(uint64_t) - off_align) > size)
+				{
+					for(const uint8_t tpoint : p_data)
+					{
+						p_current = _mm_crc32_u8(p_current, tpoint);
+					}
+					return p_current;
+				}
+
+				switch(off_align)
+				{
+				case 1:
+					p_current = _mm_crc32_u8(p_current, *pivot++);
+					[[fallthrough]];
+				case 2:
+					p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
+					pivot += 2;
+					[[fallthrough]];
+				case 4:
+					p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
+					pivot += 4;
+					break;
+				case 3:
+					p_current = _mm_crc32_u8(p_current, *pivot++);
+					p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
+					pivot += 4;
+					break;
+				case 5:
+					p_current = _mm_crc32_u8(p_current, *pivot++);
+					[[fallthrough]];
+				case 6:
+					p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
+					pivot += 2;
+					break;
+				case 7:
+					p_current = _mm_crc32_u8(p_current, *pivot++);
+					break;
+				default:
+				case 0:
+					break;
+				}
+			}
+
+			{
+				uint64_t context64 = p_current;
+				for(; (last - pivot) >= 8; pivot += 8)
+				{
+					context64 = _mm_crc32_u64(context64, *reinterpret_cast<const uint64_t*>(pivot));
+				}
+				p_current = static_cast<uint32_t>(context64);
+			}
+
+			switch(last - pivot)
+			{
+			case 7:
+				p_current = _mm_crc32_u8(p_current, *pivot++);
+				[[fallthrough]];
+			case 6:
+				p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
+				pivot += 2;
+				[[fallthrough]];
+			case 4:
+				p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
+				break;
+			case 5:
+				p_current = _mm_crc32_u8(p_current, *pivot++);
+				p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
+				break;
+			case 3:
+				p_current = _mm_crc32_u8(p_current, *pivot++);
+				[[fallthrough]];
+			case 2:
+				p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
+				break;
+			case 1:
+				p_current = _mm_crc32_u8(p_current, *pivot);
+				break;
+			default:
+			case 0:
+				break;
+			}
+
+			return p_current;
+		}
+
+		static uint32_t trasform_a_intri(const uint32_t p_current, const std::span<const uint64_t> p_data)
 		{
-			context64 = _mm_crc32_u64(context64, *reinterpret_cast<const uint64_t*>(pivot));
+			uint64_t context64 = p_current;
+			for(const uint64_t tpoint : p_data)
+			{
+				context64 = _mm_crc32_u64(context64, tpoint);
+			}
+			return static_cast<uint32_t>(context64);
 		}
-		p_current = static_cast<uint32_t>(context64);
-	}
 
-	switch(last - pivot)
-	{
-	case 7:
-		p_current = _mm_crc32_u8(p_current, *pivot++);
-		[[fallthrough]];
-	case 6:
-		p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
-		pivot += 2;
-		[[fallthrough]];
-	case 4:
-		p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
-		break;
-	case 5:
-		p_current = _mm_crc32_u8(p_current, *pivot++);
-		p_current = _mm_crc32_u32(p_current, *reinterpret_cast<const uint32_t*>(pivot));
-		break;
-	case 3:
-		p_current = _mm_crc32_u8(p_current, *pivot++);
-		[[fallthrough]];
-	case 2:
-		p_current = _mm_crc32_u16(p_current, *reinterpret_cast<const uint16_t*>(pivot));
-		break;
-	case 1:
-		p_current = _mm_crc32_u8(p_current, *pivot);
-		break;
-	default:
-	case 0:
-		break;
-	}
 
-	return p_current;
+		using unaligned_cb_t = uint32_t (*)(uint32_t, const std::span<const uint8_t>);
+		using aligned_cb_t =  uint32_t (*)(uint32_t, const std::span<const uint64_t>);
+
+		static unaligned_cb_t const trasform_unaligned;// = core::amd64::CPU_feature_su::SSE42() ? trasform_u_intri : trasform_u_soft;
+		static aligned_cb_t const   trasform_aligned;// = core::amd64::CPU_feature_su::SSE42() ? trasform_a_intri : trasform_a_soft;
+
+#else
+		static inline uint32_t trasform_unaligned(uint32_t p_current, const std::span<const uint8_t> p_data)
+		{
+			return trasform_u_soft(p_current, p_data);
+		}
+
+		static inline uint32_t trasform_aligned(const uint32_t p_current, const std::span<const uint64_t> p_data)
+		{
+			return trasform_a_soft(p_current, p_data);
+		}
+#endif
+	};
+
+#if defined(_M_AMD64) || defined(__amd64__)
+	CRC_32C_Help::unaligned_cb_t const CRC_32C_Help::trasform_unaligned = core::amd64::CPU_feature_su::SSE42() ? CRC_32C_Help::trasform_u_intri : CRC_32C_Help::trasform_u_soft;
+	CRC_32C_Help::aligned_cb_t const   CRC_32C_Help::trasform_aligned   = core::amd64::CPU_feature_su::SSE42() ? CRC_32C_Help::trasform_a_intri : CRC_32C_Help::trasform_a_soft;
+#endif
+
+} //namespace
+
+
+uint32_t CRC_32C::trasform(uint32_t p_current, const std::span<const uint8_t> p_data)
+{
+	return CRC_32C_Help::trasform_unaligned(p_current, p_data);
 }
 
 uint32_t CRC_32C::trasform(const uint32_t p_current, const std::span<const uint64_t> p_data)
 {
-	uint64_t context64 = p_current;
-	for(const uint64_t tpoint : p_data)
-	{
-		context64 = _mm_crc32_u64(context64, tpoint);
-	}
-	return static_cast<uint32_t>(context64);
+	return CRC_32C_Help::trasform_aligned(p_current, p_data);
 }
-
-#else
-
-CRC_32C::digest_t CRC_32C::trasform(digest_t p_current, const std::span<const uint8_t> p_data)
-{
-	static_assert(std::is_same_v<digest_t, uint32_t>);
-	using CRC_Helper = CRC_Help<digest_t, poly, reciprocal>;
-
-	const uintptr_t			size  = p_data.size();
-	const uint8_t*			pivot = p_data.data();
-	const uint8_t* const	last  = pivot + size;
-
-	{
-		const uintptr_t off_align = align_t_mod<uint32_t>(pivot);
-		if((alignof(uint32_t) - off_align) > size)
-		{
-			for(const uint8_t tpoint : p_data)
-			{
-				p_current = CRC_Helper::soft_byte(p_current, tpoint);
-			}
-			return p_current;
-		}
-
-		switch(off_align)
-		{
-		case 1:
-			p_current = CRC_Helper::soft_byte(p_current, *pivot++);
-			[[fallthrough]];
-		case 2:
-			p_current = CRC_Helper::soft_byte(p_current, *pivot++);
-			[[fallthrough]];
-		case 3:
-			p_current = CRC_Helper::soft_byte(p_current, *pivot++);
-			break;
-		default:
-		case 0:
-			break;
-		}
-	}
-
-	for(; (last - pivot) >= 4; pivot += 4)
-	{
-		p_current = CRC_Helper::soft_multi_byte(p_current, *reinterpret_cast<const uint32_t*>(pivot));
-	}
-
-	switch(last - pivot)
-	{
-	case 3:
-		p_current = CRC_Helper::soft_byte(p_current, *pivot++);
-		[[fallthrough]];
-	case 2:
-		p_current = CRC_Helper::soft_byte(p_current, *pivot++);
-		[[fallthrough]];
-	case 1:
-		p_current = CRC_Helper::soft_byte(p_current, *pivot);
-		break;
-	default:
-	case 0:
-		break;
-	}
-
-	return p_current;
-}
-
-CRC_32C::digest_t CRC_32C::trasform(digest_t p_current, const std::span<const uint64_t> p_data)
-{
-	static_assert(std::is_same_v<digest_t, uint32_t>);
-	using CRC_Helper = CRC_Help<digest_t, poly, reciprocal>;
-	for(const uint64_t tpoint : p_data)
-	{
-		p_current = CRC_Helper::soft_multi_byte(p_current, *reinterpret_cast<const uint32_t*>(&tpoint));
-		p_current = CRC_Helper::soft_multi_byte(p_current, *(reinterpret_cast<const uint32_t*>(&tpoint) + 1));
-	}
-	return p_current;
-}
-#endif
-
 
 CRC_64::digest_t CRC_64::trasform(digest_t p_current, const std::span<const uint8_t> p_data)
 {
